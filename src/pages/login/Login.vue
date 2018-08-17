33d4e5c91586b3701683ff49650e4cbabe1eab29
3127148186@qq.com <3127148186@qq.com>
2018-08-17 17:44:45 +0800

    validations+enter submit +imgChange+btn disable

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -4,34 +4,40 @@
     <canvas id="canvas"></canvas>
     <!--表单-->
     <el-container>
-      <el-form :model="loginForm" :rules="rules" class="loginForm">
+      <el-form :model="loginForm" :rules="rules" class="loginForm" ref="loginForm">
         <el-form-item>
           <h1>饿？登录解决温饱！</h1>
         </el-form-item>
         <el-form-item prop="tel">
-          <el-input v-model="loginForm.tel" placeholder="请输入正确的电话号码" clearable></el-input>
+          <el-input v-model="loginForm.tel" placeholder="请输入正确的电话号码" clearable @focus="changeMethod" @change="freeBtn(loginForm.tel)" maxlength="11"></el-input>
         </el-form-item>
         <el-form-item prop="msg" >
-          <el-input v-model="loginForm.msg" placeholder="请输入收到的验证码" clearable></el-input>
+          <el-input v-model="loginForm.msg" placeholder="请输入收到的验证码" clearable @focus="changeMethod" @keyup.13.native="submitForm('loginForm')"></el-input>
         </el-form-item>
         <el-form-item>
               温馨提示：未注册'不饿吗'账号的手机号，登录时将自动注册，且代表您已同意
               <a href="https://baike.baidu.com/item/%E7%99%BE%E5%BA%A6%E7%94%A8%E6%88%B7%E5%8D%8F%E8%AE%AE/2602112">不饿吗用户协议</a>
         </el-form-item>
         <el-form-item  width="100%">
-          <el-button type="success" @click="submitForm('loginForm')">登录</el-button>
+          <el-button @click="submitForm('loginForm')">登录</el-button>
         </el-form-item>
       </el-form>
+      <el-button @click="sendMsg" :disabled="btn" >获取验证码</el-button>
+      <img src="https://b-gold-cdn.xitu.io/v3/static/img/greeting.1415c1c.png" class="g-tel" v-show="defaultImg">
+      <img src="https://b-gold-cdn.xitu.io/v3/static/img/blindfold.58ce423.png" class="g-msg" v-show="changeImg">
     </el-container>
   </div>
 </template>
 
 <script>
   import CanvasBG from '../../js/canvas.js'
-  //  $vm
+
   export default {
     data() {
       return {
+        btn:true,
+        defaultImg:false,
+        changeImg:true,
         timeId:null,
         loginForm: {
           tel: '',
@@ -43,22 +49,55 @@
             {min: 11, max: 11, message: '手机号是11位哒~', trigger: 'blur'}
           ],
           msg: [
-            {required: true, message: '短信收到了没啊', trigger: 'blur'}
+            {required: true, message: '验证码收到了没啊~', trigger: 'blur'}
           ]
         }
       };
     },
     methods: {
       submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
+        this.$refs[formName].validate(async (valid) => {
           if (valid) {
-            alert('submit!');
+            if (this.loginForm.msg == 'starve') {
+              this.$message({
+                type: 'success',
+                message:'让我们开始饕餮之旅吧~~~'
+              });
+              this.$router.push("/");
+            }else {
+              this.$message({
+                type: 'error',
+                message:'emmmmm...验证码不对哦'
+              });
+              this.$refs[formName].resetFields();
+              this.btn=true;
+            }
           } else {
-              console.log('error submit!!');
-          return false;
-        }
-      });
+            this.$message({
+              type: 'error',
+              message:'别心急，先完成验证嘛~~'
+            });
+            return false;
+          }
+        });
       },
+      freeBtn(value){
+        if(value.length==11) {
+            this.btn=false;
+        }else {
+            this.btn=true;
+        }
+      },
+      changeMethod() {
+        this.defaultImg = !this.defaultImg;
+        this.changeImg = !this.changeImg;
+      },
+      sendMsg() {
+        this.$message({
+          type: 'warning',
+          message:'验证码：starve'
+        });
+      }
     },
     mounted() {
 //      使用canvas动态背景的2种方法：1. import CanvasBG from '../../js/canvas.js'后直接调用；2.在mounted直接写
@@ -180,6 +219,7 @@
       position : absolute;
     }
   }
+
  .el-container {
     width:300px;
     height:500px;
@@ -192,6 +232,13 @@
    transform: translate(-50%,-50%);
    /*background-color: #fff;*/
 
+    &> button {
+       position: relative;
+       top: 133px;
+       left: -108px;
+       height: 41px;
+       width: 108px !important;
+     }
       .el-form-item:nth-child(4) {
         /deep/ div {
             padding-top: 10px;
@@ -200,12 +247,25 @@
       }
 
       h1 {
-        margin-bottom: 100px;
+        font-size: 33px;
+        margin-bottom: 72px;
         text-align: center;
       }
       button {
         width: 300px;
+        border: none;
+        color: #fff;
+        background-color:#61daa5;
       }
+  }
+
+  img {
+    position: absolute;
+    top: 60px;
+    left: 50%;
+    width: 120px;
+    height: 95px;
+    transform: translate(-50%, 0);
   }
 
 </style>
